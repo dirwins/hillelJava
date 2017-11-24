5f3797881325cdbc11d8f285684a81daad83449c
Dmytro <mailtgr@gmail.com>
2017-11-25 01:44:34 +0200

    Add checking Zero-division

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,27 +1,50 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 
 public class Calculator {
     public static void main(String[] args){
-        int firstNumber;    //
-        int secondNumber;   //
-        Scanner getNumber = new Scanner(System.in);
+        double firstNumber, secondNumber;
+
+        Scanner getNumber = new Scanner(System.in);     // Reading input from command-line
+        getNumber.useLocale(Locale.ENGLISH);
+
 
         System.out.println("Enter Firts number: ");
-        firstNumber = getNumber.nextInt();
+        firstNumber = getNumber.nextDouble();
+        System.out.println("Your input " + firstNumber + " as a first number.");
 
         System.out.println("Enter Second number: ");
-        secondNumber = getNumber.nextInt();
+        secondNumber = getNumber.nextDouble();
 
-        int sum = firstNumber + secondNumber;
-        int difference = firstNumber - secondNumber;
-        int multiplication = firstNumber * secondNumber;
-        int division = firstNumber / secondNumber;
+        System.out.println("Your input " + secondNumber + " as a second number.");
 
-        System.out.println("Sum of First Number and Second Number is " + sum);  // Output: Sum int and int
-        System.out.println("Difference of First Number and Second Number is " + difference);    // Output: Difference int and int
-        System.out.println("Multiplication of First Number and Second Number is " + multiplication);    // Output: Multiplication int and int
-        System.out.println("Division of First Number and Second Number is " + division);    // Output: Division int and int
+        System.out.println();
+        System.out.println("Your: ");
+        double sum = firstNumber + secondNumber;        // Sum
+        System.out.println("Sum is " + sum);    // Output: Sum double and double
+
+        double difference = firstNumber - secondNumber;     // Difference
+        System.out.println("Difference is " + difference);    // Output: Difference double and double
+
+        double multiplication = firstNumber * secondNumber;     // Multiplication
+        System.out.println("Multiplication is " + multiplication);    // Output: Multiplication double and double
+
+        System.out.print("Division is ");   // Output: Division double and double
+        if(secondNumber != 0) {             //Check zero-division
+            double division = firstNumber / secondNumber;    // Division
+            System.out.println(division);    // Output: Division double and double
+        }else{
+            System.out.println("ERROR");
+            System.out.println();
+            System.out.println("Division Error! Second input equals Zero. Try again with other number");
+        }
+
+
+
+
+
+
 
     }
 }
