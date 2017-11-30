165e5cd3121a6bd8338ac981fc816bb3697038ed
Dmytro <mailtgr@gmail.com>
2017-11-30 03:26:15 +0200

    Add solving tasks as: - Modified calculator - Banking deposit - Search for the average (2 solves) - Three graphical tasks - Search for the Odd/Even number, Prime number, Multiple number

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,50 +3,96 @@ import java.util.Scanner;
 
 
 public class Calculator {
+    private static Scanner getNumber = new Scanner(System.in);
 
-    private static void getN(Scanner getNumber) {
+
+    private static double getNcheck() { // Get number from Console
+        getNumber.useLocale(Locale.ENGLISH);
+        double num;
+        System.out.println("Enter number:");
         if (!getNumber.hasNextDouble()) {      // Check numerical type firstNumber
             System.out.println("Non-numerical type. Try again.");
-            System.exit(0);
+            getNumber.next();
+            num = getNcheck();
+        } else {
+            num = getNumber.nextDouble();
         }
+        return num;
     }
 
-    public static void main(String[] args){
-        double firstNumber, secondNumber;
-
-        Scanner getNumber = new Scanner(System.in);     // Reading input from command-line
-        getNumber.useLocale(Locale.ENGLISH);
-
-        System.out.println("Enter First number: ");
-        getN(getNumber);
-        firstNumber = getNumber.nextDouble();
-        System.out.println("Your input " + firstNumber + " as a first number.");
-        System.out.println("Enter Second number: ");
-
-        getN(getNumber);
-        secondNumber = getNumber.nextDouble();
-        System.out.println("Your input " + secondNumber + " as a second number.");
-
-        System.out.println();
-        System.out.println("Your: ");
-        double sum = firstNumber + secondNumber;        // Sum
-        System.out.println("Sum is " + sum);    // Output: Sum double and double
-
-        double difference = firstNumber - secondNumber;     // Difference
-        System.out.println("Difference is " + difference);    // Output: Difference double and double
-
-        double multiplication = firstNumber * secondNumber;     // Multiplication
-        System.out.println("Multiplication is " + multiplication);    // Output: Multiplication double and double
-
-        System.out.print("Division is ");   // Output: Division double and double
-        if(secondNumber != 0) {             //Check zero-division
-            double division = firstNumber / secondNumber;    // Division
-            System.out.println(division);    // Output: Division double and double
-        }else{
-        System.out.println("ERROR");
-        System.out.println();
-        System.out.println("Division Error! Second input equals Zero. Try again with other number");
+    private static char getAction() { // Get action from Console
+        System.out.println("Enter an action(operation):");
+        char operation;
+        if (getNumber.hasNext()) {
+            operation = getNumber.next().charAt(0);
+        } else {
+            System.out.println("Error. Wrong operation!");
+            operation = getAction();
         }
+        return operation;
     }
 
+
+    private static double Calc(double N1, double N2, char operation) { // Calculate result
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = SumCalc(N1, N2);
+                System.out.println("Sum is " + result);
+                break;
+            case '-':
+                result = DifferenceCalc(N1, N2);
+                System.out.println("Difference is " + result);
+                break;
+            case '*':
+                result = MultiplicationCalc(N1, N2);
+                System.out.println("Multiplication is " + result);
+                break;
+            case '/':
+                result = DivisionCalc(N1, N2);
+                System.out.print("Division is " + result);
+                break;
+            default:
+                System.out.println("Unavailable action. Try again.");
+                result = Calc(N1, N2, getAction());
+        }
+        return result;
+
+    }
+
+    private static double SumCalc(double firstN, double secondN) {
+        double Sum;
+        Sum = firstN + secondN;
+        return Sum;
+    }
+
+    private static double DifferenceCalc(double firstN, double secondN) {
+        double Difference;
+        Difference = firstN - secondN;
+        return Difference;
+    }
+
+    private static double MultiplicationCalc(double firstN, double secondN) {
+        double Multiplication;
+        Multiplication = firstN * secondN;
+        return Multiplication;
+    }
+
+    private static double DivisionCalc(double firstN, double secondN) {
+        double Division;
+        while (secondN == 0) { // Сheck the division by zero
+            System.out.println("Division Error! Second input equals Zero. Try again with other number");
+            secondN = getNcheck();
+        }
+        Division = firstN / secondN;
+        return Division;
+    }
+
+
+    public static void main(String[] args) {
+        double getN1 = getNcheck(); // Get first number
+        double getN2 = getNcheck(); // Get second number
+        Calc(getN1, getN2, getAction()); // Calculate result
+    }
 }
